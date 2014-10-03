925887434ff08339bc9a518d4e74eb3a9dd99b5b
freshProgrammer <dougie+github@freshprogramming.com>
2014-10-03 10:13:00 -0700

    Added player move function and start of facing direction code.

--- gameCode/Player.js
+++ gameCode/Player.js
@@ -4,7 +4,7 @@ Player.prototype = new GameObject();        // Here's where the inheritance occu
 Player.prototype.constructor=Player;  
 function Player()
 {
-	
+	this.facingDir = new Point(0,0);
 }
 
 Player.prototype.update=function(time)
@@ -12,9 +12,24 @@ Player.prototype.update=function(time)
 	
 };
 
+Player.prototype.move=function(dx, dy)
+{
+	this.X+=dx;
+	this.Y+=dy;
+	this.facingDir.X = dx;
+	this.facingDir.Y = dy;
+};
+
 Player.prototype.draw=function(context)
 {
-	context.drawImage(Player.image, this.X, this.Y);
+	var radRotation = Math.atan2(this.facingDir.Y, this.facingDir.X);
+
+	context.save();
+	context.translate(this.X+this.Width/2,this.Y+this.Height/2);//translate to center of object for rotation around center
+	context.rotate(radRotation);
+	
+	context.drawImage(Player.image, 0, 0);
+	context.restore();
 };
 
 Player.prototype.toString=function()
